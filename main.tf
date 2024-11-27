30c393ac6d815b7a37f202b0e548b07fb6a86d27
Yashu367 <ynavulur@charlotte.edu>
2024-11-27 17:09:11 -0500

    main code updated

--- main.tf
+++ main.tf
@@ -30,6 +30,23 @@ resource "azurerm_resource_group" "rg" {
   tags = local.tags
 }
 
+// Virtual Network Within Resource Group
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-dsba6190-yashu367-dev-eastus-877"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "snet" {
+  name                 = "snet-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
 
 // Storage Account
 
@@ -40,5 +57,49 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.snet.id]
+  }
+
   tags = local.tags
+}
+
+//SQL SERVER AND DATABASE
+
+resource "azurerm_mssql_server" "sser" {
+  name                         = "ssr-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_virtual_network_rule" "sql_vnet_rule" {
+  name                                 = "vnet-rule-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  server_id                            = azurerm_mssql_server.sser.id
+  subnet_id                            = azurerm_subnet.snet.id
+  ignore_missing_vnet_service_endpoint = false
+}
+
+
+resource "azurerm_mssql_database" "sdb" {
+  name         = "db-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  server_id    = azurerm_mssql_server.sser.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
 }
--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "cford38"
+  default     = "yashu367"
 }
 
 variable "class_name" {
